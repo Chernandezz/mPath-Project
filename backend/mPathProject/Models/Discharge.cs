ba42452545358d957a652d93e2be84086c9e8d53
Cristian Hernandez <crissstianfhz@gmail.com>
2025-01-28 09:06:51 -0500

    Discharge model decimal fixed

--- backend/mPathProject/Models/Discharge.cs
+++ backend/mPathProject/Models/Discharge.cs
@@ -1,21 +1,35 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace mPathProject.Models
 {
     public class Discharge
     {
         public long id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string treatment { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string doctorName { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string patientName { get; set; }
 
         [Required]
         public System.DateTime dischargeDate { get; set; }
+
         [Required]
+        [Column(TypeName = "decimal(18,2)")] 
         [Range(0, 9999999.99)]
         public decimal amount { get; set; }
+
         [Required]
         public long doctorId { get; set; }
+
         [Required]
         public long admissionId { get; set; }
     }
